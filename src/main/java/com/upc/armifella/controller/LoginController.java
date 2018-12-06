a3acceca789068c69157b21f68f912d7d5acdf30
Arly Fernandez <arly.miguel.fernandez@gmail.com>
2018-12-06 11:45:52 -0500

    fixed

--- src/main/java/com/upc/armifella/controller/LoginController.java
+++ src/main/java/com/upc/armifella/controller/LoginController.java
@@ -1,7 +1,7 @@
 package com.upc.armifella.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("/authenticate")
+	@PostMapping("/authenticate")
 	public boolean login(@RequestBody User userRequest) {
 		return userService.login(userRequest);
 	}
